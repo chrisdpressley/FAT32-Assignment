4c028a11ab447036e5fdcd837bd3a74c9f0a9d4b
cdp7684 <cdp7684@mavs.uta.edu>
2024-11-02 06:43:24 +0000

    save working somewhat

--- mfs.c
+++ mfs.c
@@ -14,6 +14,7 @@ struct FAT32_Boot{            //Offsets:
   int16_t BPB_RsvdSecCnt;    // 14
   int8_t BPB_NumFATS;        // 16
   int16_t BPB_RootEntCnt;    // 17
+  int32_t BPB_TotSec32;      // 34
   int16_t BPB_FATSz32;       // 36
   int16_t BPB_ExtFlags;      // 40
   int32_t BPB_RootClus;      // 44
@@ -24,6 +25,30 @@ struct FAT32_Boot{            //Offsets:
   int32_t FirstSectorofCluster;
 };
 
+struct DirectoryEntry {
+  char DIR_Name[12];
+  int8_t DIR_Attr;
+  int8_t Unused[8];
+  int16_t DIR_FirstClusterHigh;
+  int8_t Unused2[4];
+  int16_t DIR_FirstClusterLow;
+  int32_t DIR_FileSize;
+};
+
+void load_records(struct DirectoryEntry *d,FILE *fp)
+{
+  for(int i = 0;i < 16;i++)
+  {
+    fread(&d[i].DIR_Name,11,1,fp);
+    d[i].DIR_Name[11] = '\0';
+    fread(&d[i].DIR_Attr,1,1,fp);
+    fseek(fp,8,SEEK_CUR);
+    fread(&d[i].DIR_FirstClusterHigh,2,1,fp);
+    fseek(fp,4,SEEK_CUR);
+    fread(&d[i].DIR_FirstClusterLow,2,1,fp);
+    fread(&d[i].DIR_FileSize,4,1,fp);
+  }
+}
 
 void load_bootSector_values(struct FAT32_Boot * b,FILE *fp)
 {
@@ -39,6 +64,8 @@ void load_bootSector_values(struct FAT32_Boot * b,FILE *fp)
   fread(&b->BPB_NumFATS,1,1,fp);
   fseek(fp,17,SEEK_SET);
   fread(&b->BPB_RootEntCnt,2,1,fp);
+  fseek(fp,32,SEEK_SET);
+  fread(&b->BPB_TotSec32,4,1,fp);
   fseek(fp,36,SEEK_SET);
   fread(&b->BPB_FATSz32,2,1,fp);
   fseek(fp,40,SEEK_SET);
@@ -77,11 +104,54 @@ char * get_fat_file_name(char * token)
 }
 
 
+void print_records(struct DirectoryEntry *d)
+{
+  for(int i = 0;i<16;i++)
+  {
+    if(d[i].DIR_Attr == 0x01 || d[i].DIR_Attr == 0x10 || d[i].DIR_Attr == 0x20 )
+    {
+      printf("%d. DIR_FileName: |%s|\n",i,d[i].DIR_Name);
+      printf("%d. DIR_Attr: %X\n",i,d[i].DIR_Attr);
+      printf("%d. DIR_FirstClusterHigh: %X\n",i,d[i].DIR_FirstClusterHigh);
+      printf("%d. DIR_FirstClusterLow: %X\n",i,d[i].DIR_FirstClusterLow);
+      printf("%d. DIR_FileSize: %d\n",i,d[i].DIR_FileSize);
+    }
+  }
+}
+
+int LBAToOffset(int32_t sector,struct FAT32_Boot * b)
+{
+  return((sector - 2) * b->BPB_BytesPerSec) + (b->BPB_BytesPerSec * b->BPB_RsvdSecCnt) + (b->BPB_NumFATS * b->BPB_FATSz32 * b->BPB_BytesPerSec);
+}
+
+int16_t NextLB(uint32_t sector,struct FAT32_Boot * b,FILE *fp)
+{
+  uint32_t FATAddress = (b->BPB_BytesPerSec * b->BPB_RsvdSecCnt) + (sector * 4);
+  int16_t val;
+  fseek(fp, FATAddress,SEEK_SET);
+  fread(&val, 2, 1, fp);
+  return val;
+}
+void get_stat_info(struct DirectoryEntry *d,char * searchName)
+{
+  for(int i = 0;i<16;i++)
+  {
+    if(strcmp(d[i].DIR_Name,searchName) == 0)
+    {
+      printf("Attribute: %X\nStarting Cluster Number: %X%X\n",d[i].DIR_Attr,d[i].DIR_FirstClusterHigh,d[i].DIR_FirstClusterLow);
+      return;
+    }
+  }
+}
+
+
 int main( int argc, char * argv[] )
 {
   FILE *fp = NULL;
   struct FAT32_Boot boot;
-  char name[9],ext[4];
+  struct DirectoryEntry dir[16]; 
+  char *currentImage = NULL;
+  int imageSize; 
   for( ; ; )
   {
     if(fp != NULL)
@@ -89,12 +159,13 @@ int main( int argc, char * argv[] )
       fseek(fp,((boot.BPB_NumFATS * boot.BPB_FATSz32 * boot.BPB_BytesPerSec) 
             + (boot.BPB_RsvdSecCnt * boot.BPB_BytesPerSec)),SEEK_SET);
     }
-    char imgName[12];
+    char secondArg[12];
     char * command_string = (char*) malloc( MAX_COMMAND_SIZE );
     char *argument_pointer;
     char *token[MAX_NUM_ARGUMENTS];
-    //char executePath[MAX_COMMAND_SIZE];
     int token_count = 0;
+    
+
     printf("mfs> ");
     fgets (command_string, MAX_COMMAND_SIZE, stdin);
     while ( ( (argument_pointer = strsep(&command_string, " \t\n" ) ) != NULL) &&
@@ -107,51 +178,85 @@ int main( int argc, char * argv[] )
       token[token_count] = strndup( argument_pointer, MAX_COMMAND_SIZE );
       token_count++;
     }
+
+
     if(strcmp(token[0],"exit") == 0 || strcmp(token[0],"quit") == 0)
     {
       return 0;
     }
+
+
     if(token[1] != NULL)
     {
-      strcpy(imgName,token[1]);
+      strcpy(secondArg,token[1]);
     }
+
     if(strcmp(token[0],"open") == 0)
     {
       if(fp == NULL)
       {
-        fp = fopen(imgName,"r");
+        fp = fopen(secondArg,"r");
         if(fp == NULL)
         {
           printf("File not found");
           continue;
         }
+        currentImage = strdup(secondArg);
         load_bootSector_values(&boot,fp);
+        
+        fseek(fp,((boot.BPB_NumFATS * boot.BPB_FATSz32 * boot.BPB_BytesPerSec) 
+            + (boot.BPB_RsvdSecCnt * boot.BPB_BytesPerSec)),SEEK_SET);
+        load_records(dir,fp);
+        print_records(dir);
+        imageSize = ((boot.BPB_NumFATS * boot.BPB_FATSz32 * boot.BPB_BytesPerSec) //Size of rsvd sec + size of fats + (size of sec * num clusters)
+                  + (boot.BPB_RsvdSecCnt * boot.BPB_BytesPerSec) + (boot.BPB_SecPerClus * boot.BPB_BytesPerSec) //spent at least an hour figuring this out
+                  * (boot.BPB_TotSec32 - (boot.BPB_RsvdSecCnt + (boot.BPB_NumFATS * boot.BPB_FATSz32))));
+        printf("%d\n",imageSize);
       }
       else
       {
         printf("File already open.");
-        continue;
       }
     }
     else if(strcmp(token[0],"save") == 0)
     {
-      
+      if(strlen(token[1]) != 0)
+      {
+        printf("%s\n",currentImage);
+        FILE *outputFile = fopen(secondArg,"wb");
+        FILE *inputFile = fopen(currentImage,"rb");
+        char *buffer = malloc(imageSize);
+        fseek(fp,0,SEEK_SET);
+        fread(buffer,1,imageSize,inputFile);
+        size_t written = fwrite(buffer, 1, imageSize, outputFile);
+        if(written == imageSize)
+        {
+          fclose(inputFile);
+          fclose(outputFile);
+        }
+        
+      }
+      else
+      {
+
+      }
     }
     else if(strcmp(token[0],"info") == 0)
     {
-      
-      printf("BPB_BytesPerSec: \t%d\t%X\n",boot.BPB_BytesPerSec,boot.BPB_BytesPerSec);
-      printf("BPB_SecPerClus: \t%d\t%X\n",boot.BPB_SecPerClus,boot.BPB_SecPerClus);
-      printf("BPB_RsvdSecCnt: \t%d\t%X\n",boot.BPB_RsvdSecCnt,boot.BPB_RsvdSecCnt);
-      printf("BPB_NumFATS: \t\t%d\t%X\n",boot.BPB_NumFATS,boot.BPB_NumFATS);
-      printf("BPB_FATSz32: \t\t%d\t%X\n",boot.BPB_FATSz32,boot.BPB_FATSz32);
-      printf("BPB_ExtFlags: \t\t%d\t%X\n",boot.BPB_ExtFlags,boot.BPB_ExtFlags);
-      printf("BPB_RootClus: \t\t%d\t%X\n",boot.BPB_RootClus,boot.BPB_RootClus);
-      printf("BPB_BPB_FSInfo: \t%d\t%X\n",boot.BPB_FSInfo,boot.BPB_FSInfo);
+      printf("BPB_BytesPerSec:\t%d\t%X\n",boot.BPB_BytesPerSec,boot.BPB_BytesPerSec);
+      printf("BPB_SecPerClus:\t%d\t%X\n",boot.BPB_SecPerClus,boot.BPB_SecPerClus);
+      printf("BPB_RsvdSecCnt:\t%d\t%X\n",boot.BPB_RsvdSecCnt,boot.BPB_RsvdSecCnt);
+      printf("BPB_NumFATS:\t\t%d\t%X\n",boot.BPB_NumFATS,boot.BPB_NumFATS);
+      printf("BPB_FATSz32:\t\t%d\t%X\n",boot.BPB_FATSz32,boot.BPB_FATSz32);
+      printf("BPB_ExtFlags:\t\t%d\t%X\n",boot.BPB_ExtFlags,boot.BPB_ExtFlags);
+      printf("BPB_RootClus:\t\t%d\t%X\n",boot.BPB_RootClus,boot.BPB_RootClus);
+      printf("BPB_FSInfo:\t\t%d\t%X\n",boot.BPB_FSInfo,boot.BPB_FSInfo);
     }
     else if(strcmp(token[0],"stat") == 0)
     {
-      
+      //strcpy(secondArg,token[1]);
+      strcpy(secondArg,get_fat_file_name(token[1]));
+      get_stat_info(dir,secondArg);
     }
     else if(strcmp(token[0],"get") == 0)
     {
@@ -167,23 +272,12 @@ int main( int argc, char * argv[] )
     }
     else if(strcmp(token[0],"ls") == 0)
     {
-      fseek(fp,((boot.BPB_NumFATS * boot.BPB_FATSz32 * boot.BPB_BytesPerSec) 
-            + (boot.BPB_RsvdSecCnt * boot.BPB_BytesPerSec)),SEEK_SET);
-      for(int i = 0;i<16;i++)
+      for(int i = 0;i < 16;i++)
       {
-        int8_t attr;
-        fseek(fp,11,SEEK_CUR);
-        fread(&attr,1,1,fp);
-        fseek(fp,-12,SEEK_CUR);
-        if(attr == 0x01 || attr == 0x10 || attr == 0x20)
+        if(dir[i].DIR_Attr == 0x01 || dir[i].DIR_Attr == 0x10 || dir[i].DIR_Attr == 0x20 )
         {
-          fread(name,8,1,fp);
-          fread(ext,3,1,fp);
-          name[8] = '\0';
-          ext[3] = '\0';
-          printf("%8s%3s\n",name,ext);
+          printf("%s\n",dir[i].DIR_Name);
         }
-        fseek(fp,21,SEEK_CUR);
       }
     }
     else if(strcmp(token[0],"read") == 0)
